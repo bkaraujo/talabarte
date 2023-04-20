f84c7e4e689c415b5ba2b1cd526afd0313199115
Bruno da Silva Perez Araújo <bkraujo@outlook.com>
2023-04-20 14:54:52 -0300

    ...

--- engine.ps1
+++ engine.ps1
@@ -11,7 +11,7 @@ New-Item $BINFS -ItemType Directory | Out-Null
 # Compile library
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 $COMMAND = "C:\'Program Files'\LLVM\bin\clang.exe"
-$COMMAND += " -g -shared -Wall -Werror -Wno-incompatible-pointer-types" 
+$COMMAND += " -g -shared -Wall -Werror" 
 $COMMAND += " -DEXPORT -DUNICODE -D_UNICODE"
 $COMMAND += " $(Get-ChildItem -Path $SRCFS -Recurse -Include *.c)"
 $COMMAND += " -I$SRCFS"
